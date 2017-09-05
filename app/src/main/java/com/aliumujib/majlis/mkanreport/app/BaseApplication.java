b7d1e35d8f1dc508a8dcb90aca5ad6e997d645ae
Abdul-Mujeeb Aliu <abdulmujibaliu@Abdul-Mujeebs-MacBook-Pro.local>
2017-09-05 22:32:25 +0100

    Added bolts

--- app/src/main/java/com/aliumujib/majlis/mkanreport/app/BaseApplication.java
+++ app/src/main/java/com/aliumujib/majlis/mkanreport/app/BaseApplication.java
@@ -6,17 +6,20 @@ import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
 import com.alium.mkan_report_data.constants.Constants;
+import com.aliumujib.majlis.mkanreport.R;
 import com.aliumujib.majlis.mkanreport.utils.SharedHelper;
+import com.crashlytics.android.Crashlytics;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 
 import butterknife.ButterKnife;
 import in.myinnos.customfontlibrary.TypefaceUtil;
+import io.fabric.sdk.android.Fabric;
 
 public class BaseApplication extends MultiDexApplication {
 
     protected static BaseApplication instance;
-    protected static  String TAG = BaseApplication.class.getSimpleName();
+    protected static String TAG = BaseApplication.class.getSimpleName();
     private SharedHelper mAppSharedPrefHelper;
 
     public static BaseApplication getInstance() {
@@ -27,6 +30,7 @@ public class BaseApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         instance = this;
         reInitPArse(instance);
 
@@ -35,16 +39,15 @@ public class BaseApplication extends MultiDexApplication {
         ButterKnife.setDebug(true);
     }
 
-    public static Context getContext(){
+    public static Context getContext() {
         return getInstance();
     }
 
-
-    private static void reInitPArse(Context context) {
+    private void reInitPArse(Context context) {
         Parse.initialize(new Parse.Configuration.Builder(context)
-                .applicationId("cOQnuaZtNny7A9frIGyFQ4CBocOQGiTbUVBX3jEM")
-                .clientKey("hC0ktOlFf4zpzAtaILFsGtyOmZMs6wLTmQ4psKVY")
-                .server("https://parseapi.back4app.com/").enableLocalDataStore()
+                .applicationId(getString(R.string.parse_application_id))
+                .clientKey(getString(R.string.parse_client_secret))
+                .server(getString(R.string.parse_server_url)).enableLocalDataStore()
                 .build()
         );
 
@@ -69,7 +72,6 @@ public class BaseApplication extends MultiDexApplication {
         super.attachBaseContext(base);
         MultiDex.install(this);
     }
-
 
 
     public synchronized SharedHelper getmAppSharedPrefHelper() {
